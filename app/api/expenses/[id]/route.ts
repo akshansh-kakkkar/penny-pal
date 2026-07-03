806951d45ede856a4e0b786a11f69cd1b9c68b65
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-07-03 11:46:36 +0530

    feat: added backend for the CRUD operations for the expenses

--- app/api/expenses/[id]/route.ts
+++ app/api/expenses/[id]/route.ts
@@ -1,9 +1,88 @@
 import { getSession } from "@/app/lib/session";
+import {
+  createExpenseSchema,
+  updateExpenseSchema,
+} from "@/app/lib/validations/expense";
+import {
+  createExpense,
+  deleteExpense,
+  getExpenseById,
+  updateExpense,
+} from "@/app/server/expense.service";
+import { error } from "console";
 import { NextRequest, NextResponse } from "next/server";
 
-export default function GET(req : NextRequest, {params} : {params : Promise<{id : string}>}){
-    const session = getSession();
-    if(!session){
-        return NextResponse.json({message : "Unauthorized"}, {status : 401})
+export async function GET(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const session = await getSession();
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
+    const { id } = await params;
+    const expense = await getExpenseById(id, session.user.id);
+    if (!expense) {
+      return NextResponse.json(
+        { message: "Expense not found" },
+        { status: 404 },
+      );
+    }
+    return NextResponse.json(expense);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch the expense" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const session = await getSession();
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const { id } = await params;
+    const body = await req.json();
+    const parsed = updateExpenseSchema.safeParse(body);
+    if (!parsed.success) {
+      return NextResponse.json(parsed.error.flatten(), { status: 400 });
+    }
+    const expense = await updateExpense(id, parsed.data, session.user.id);
+    return NextResponse.json(expense);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to update the expense" },
+      { status: 500 },
+    );
+  }
+}
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> },
+) {
+  try {
+    const session = await getSession();
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const { id } = await params;
+    const expense = await getExpenseById(id, session.user.id);
+    if (!expense) {
+      return NextResponse.json(
+        { message: "Expesne not found" },
+        { status: 404 },
+      );
+    }
+
+    await deleteExpense(id, session.user.id);
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    NextResponse.json({ error: "Expense not found" }, { status: 404 });
+  }
 }
--- app/api/expenses/route.ts
+++ app/api/expenses/route.ts
@@ -4,33 +4,39 @@ import { createExpense, getExpense } from "@/app/server/expense.service";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  const session = await getSession();
-  if (!session) {
-    return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-  }
-  const { searchParams } = new URL(req.url);
-  const page = Number(searchParams.get("page")) || 1;
-  const limit = Number(searchParams.get("limit")) || 10;
-  const expenses = await getExpense(session.user.id, page, limit);
-  return NextResponse.json(expenses);
-}
-
-export async function POST(req:NextRequest) {
+  try {
     const session = await getSession();
-    if(!session){
-        return NextResponse.json({message : "Unauthorized"}, {status : 401});
-
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const { searchParams } = new URL(req.url);
+    const page = Number(searchParams.get("page")) || 1;
+    const limit = Number(searchParams.get("limit")) || 10;
+    const expenses = await getExpense(session.user.id, page, limit);
+    return NextResponse.json(expenses);
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to fetch all Expenses" },
+      { status: 500 },
+    );
+  }
+}
+export async function POST(req: NextRequest) {
+  try {
+    const session = await getSession();
+    if (!session) {
+      return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
     }
     const body = await req.json();
     const parsed = createExpenseSchema.safeParse(body);
-    if(!parsed.success){
-        return NextResponse.json(parsed.error.flatten(), {status : 400})
+    if (!parsed.success) {
+      return NextResponse.json(parsed.error.flatten(), { status: 400 });
     }
-    const expense = await createExpense(
-        parsed.data,
-        session.user.id
-    )
+    const expense = await createExpense(parsed.data, session.user.id);
     return NextResponse.json(expense, {
-        status : 201
-    })
+      status: 201,
+    });
+  } catch (error) {
+    NextResponse.json({ error: "Failed to post expense" }, { status: 500 });
+  }
 }
