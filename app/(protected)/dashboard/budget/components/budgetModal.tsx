a2ee479f06cf51e517c205bbb95c3352b705f930
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-07-22 04:25:19 +0530

    feat : added UI for the budget category breakdown patch

--- app/(protected)/dashboard/budget/components/budgetModal.tsx
+++ app/(protected)/dashboard/budget/components/budgetModal.tsx
@@ -1,11 +1,13 @@
 import { AnimatePresence, motion } from "framer-motion"
 import { useBudgetModal } from "@/app/store/UseBudgetModal";
-import { CircleCheck, DollarSignIcon, X } from "lucide-react";
+import { CircleCheck, DollarSignIcon, Flower, X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { ICON_MAP } from "@/app/lib/icon-map";
 import { getCurrentMonthAndYear } from "@/app/lib/date"
 import { useBudgetStore } from "@/app/store/BudgetStore";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPiggyBank } from "@fortawesome/free-solid-svg-icons";
 type BudgetForm = {
     amount: number;
     categories: {
@@ -27,12 +29,22 @@ interface BudgetData {
         }
     }[]
 }
+interface Category {
+    color: string;
+    background: string;
+    id: string;
+    icon: keyof typeof ICON_MAP;
+    name: string
+
+}
 export default function BudgetModal() {
     const { onClose, isOpen, budgetId } = useBudgetModal();
     const { month, year } = getCurrentMonthAndYear();
     const [budget, setBudget] = useState<BudgetForm>({ amount: 0, categories: [] });
     const isEditing = budgetId !== null;
-    const {setBudget : zustBudget} = useBudgetStore();
+    const { setBudget: zustBudget } = useBudgetStore();
+    const [categories, setCategories] = useState<Category[]>([]);
+
     useEffect(() => {
         if (!isOpen || !isEditing) return;
         async function loadBudget() {
@@ -59,41 +71,54 @@ export default function BudgetModal() {
 
     }, [isOpen, budgetId])
 
-    useEffect(()=>{
-        if(!isOpen || isEditing) return;
+    useEffect(() => {
+        if (!isOpen || isEditing) return;
         setBudget({
-            amount : 0,
-            categories : [],
+            amount: 0,
+            categories: [],
         })
     }, [isOpen, isEditing]);
-    
-    const handleSubmit = async ()=>{
-        try{
+
+    const handleSubmit = async () => {
+        try {
             const payload = {
                 ...budget,
                 month,
                 year
             }
-            const res = await fetch( isEditing ? `/api/budgets/${budgetId}` : `/api/budgets`, {
-                method : isEditing ? "PATCH" : "POST",
-                headers : {
-                    "Content-Type" : "application/json",
+            const res = await fetch(isEditing ? `/api/budgets/${budgetId}` : `/api/budgets`, {
+                method: isEditing ? "PATCH" : "POST",
+                headers: {
+                    "Content-Type": "application/json",
                 },
-                body : JSON.stringify(payload)
+                body: JSON.stringify(payload)
             })
-            const data =await res.json();
+            const data = await res.json();
 
-            if(!res.ok){
+            if (!res.ok) {
                 toast.error(data.message ?? "Soemthing went wrong");
                 return;
             }
             toast.success(isEditing ? "Budget updated successfully" : "Budget created successfully")
             zustBudget(data)
             onClose();
-        }catch(error){
+        } catch (error) {
             toast.error("Something went wrong")
         }
     }
+    useEffect(() => {
+        async function loadCategories() {
+            try {
+                const res = await fetch('/api/categories')
+                const data = await res.json();
+                setCategories(data);
+            }
+            catch {
+                toast.error("Failed to fetch categories")
+            }
+        }
+        loadCategories();
+    }, [])
     return (
         <AnimatePresence>
             {isOpen && (
@@ -105,18 +130,50 @@ export default function BudgetModal() {
                     className={` bg-black/20 fixed  flex inset-0 z-40 backdrop-blur-sm justify-center items-center  `}>
                     <motion.div
                         onClick={(e) => e.stopPropagation()}
-                        className="bg-white relative  lg:w-[800px] sm:w-[400px] w-[320px]   md:w-[600px] flex-col flex justify-center gap-6 items-center  z-50 rounded-3xl p-10" >
+                        className="bg-white relative  lg:w-[800px] sm:w-[400px] w-[320px]   md:w-[600px] flex-col flex gap-4   z-50 rounded-3xl p-6" >
                         <X onClick={() => onClose()} className="absolute right-4 text-[#715767] bg-[#F4D2EF] rounded-full p-1 hover:scale-[115%] top-4 cursor-pointer transition-all duration-300" />
-                        <h2 className="text-3xl text-[#715767] font-bold capitalize">
-                            {isEditing ? "Update Budget" : "Set Your Budget"}
-                        </h2>
-                        <div className="w-full relative flex justify-center items-center">
-                            <DollarSignIcon className="absolute left-12 text-[#715767]/90" size={56} strokeWidth={3} />
-                            <input value={budget.amount} onChange={(e)=>setBudget((prev)=>({
-                                ...prev, amount : Number(e.target.value)
-                            }))} placeholder="0.00" className="border-[#715767] w-full flex justify-center items-center py-1 rounded-full text-center border-4 text-[#715767] text-7xl font-bold" />
+                        <div className="flex flex-col">
+                            <h2 className="text-xl text-[#715767] font-bold capitalize">
+                                {isEditing ? "Update Your Budget" : "Set Your Budget"}
+                            </h2>
+                            <p className="flex items-center  gap-2">
+                                <span className="text-md text-[#4D4449] font-semibold">Let's grow your savings together!</span>
+                                <span className="bg-[#F4D2EF] text-[#715767] p-1 rounded-full" ><Flower /></span>
+                            </p>
                         </div>
-                        <button  onClick={handleSubmit} className="font-bold text-4xl gap-4 w-full text-center flex justify-center items-center bg-[#715767] text-white py-4 rounded-full">
+                        <div className="w-full bg-[#F4D2EF]/50 rounded-full p-2 relative flex justify-center items-center">
+                            <DollarSignIcon className="absolute left-12 text-[#4D4449]/90" size={32} strokeWidth={3} />
+                            <input value={budget.amount} onChange={(e) => setBudget((prev) => ({
+                                ...prev, amount: Number(e.target.value)
+                            }))} placeholder="0.00" className="border-[#715767] w-full flex   py-4 rounded-full text-start px-30 border-4 outline-none text-[#715767] text-2xl font-bold" />
+                            <FontAwesomeIcon icon={faPiggyBank} className="absolute right-12 text-[#4D4449]/90 text-2xl animate-bounce animate-squish" />
+
+                        </div>
+                        <h4 className="text-md mx-4 text-[#4D4449] font-medium">Category Breakdown</h4>
+
+                        <div className="flex flex-col gap-8 overflow-y-auto max-h-[300px] mx-2">
+                            {categories.map((item) => {
+                                const allocated = budget.categories.find(c => c.categoryId === item.id)
+                                const IconComponent = ICON_MAP[item.icon]
+                                return (
+                                    <div key={item.id} className="py-4 px-4 rounded-2xl flex justify-between items-center border-2 mx-2 " style={{ backgroundColor: item.background, borderColor: item.color }} >
+                                        <div className="flex flex-1 items-center gap-2 ">
+                                            <div className="flex p-2 rounded-full w-fit" style={{ backgroundColor: item.color, }}>
+                                                <IconComponent size={24} style={{ color: item.background }} />
+                                            </div>
+                                            <div className="flex gap-1 flex-1 items-start text-start flex-col">
+                                                <div className="text-lg font-bold" style={{ color: item.color }}>{item.name}</div>
+                                                <div className="bg-white w-full h-2 rounded-full" />
+                                            </div>
+                                        </div>
+                                        <div className="flex">
+                                            <input value={allocated?.amount ?? 0} className="text-xl outline-none bg-transparent w-12 text-right flex" />
+                                        </div>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                        <button onClick={handleSubmit} className="font-bold text-4xl gap-4 w-full text-center flex justify-center items-center bg-[#715767] text-white py-4 rounded-full">
                             <span>
                                 <CircleCheck size={40} strokeWidth={2.5} />
                             </span>
@@ -127,4 +184,4 @@ export default function BudgetModal() {
             )}
         </AnimatePresence>
     )
-}
+} 
